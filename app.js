1a056ff1fe8e16efa81bee7f9fa2c45d7b629f4f
jackleiker <jackleikerchile@gmail.com>
2020-09-20 22:20:55 -0400

    placing varibales in app

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+//variables
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDom = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.produts-center');
+
+
 
