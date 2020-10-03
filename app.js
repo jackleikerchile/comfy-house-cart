6eeb3468e76d908c266d1d7c35a111f6975fef67
jackleiker <jackleikerchile@gmail.com>
2020-10-03 00:17:41 -0400

    funtions async await and try

--- app.js
+++ app.js
@@ -8,6 +8,34 @@ const cartItems = document.querySelector('.cart-items');
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.produts-center');
+// cart
+let cart = [];
 
+// getting the products
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch('products.json')
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
+}
+// display products
+class UI {
 
+}
+// local storage
+class Storage {
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+
+  // get all products
+  products.getProducts().then(data => console.log(data));
+});
